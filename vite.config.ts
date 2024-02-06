71ad0e05edcb9fbb2302a48611974ec30a1dba15
manh.ld <manh.ld@zinza.com.vn>
2024-02-06 11:26:26 +0700

    Add vitejs/plugin-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    vue(),
   ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "app", "javascript"),
+      },
+    ],
+    extensions: ['.vue', '.json', '.js'],
+  },
 })
