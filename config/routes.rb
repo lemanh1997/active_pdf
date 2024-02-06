d2a8d63496aa367d63867b2a55e6fca3685d99fd
manh.ld <manh.ld@zinza.com.vn>
2024-02-06 10:23:32 +0700

    Define two different entrypoints: panel and website

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   mount Sidekiq::Web => "/sidekiq"
 
@@ -14,4 +7,10 @@ Rails.application.routes.draw do
   # authenticate :user, lambda { |u| u.admin? } do
   #   mount Sidekiq::Web => '/sidekiq'
   # end
+
+  # Defines the root path route ("/")
+  root to: "application#website"
+
+  get "/panel(/*path)", to: "application#panel", as: :panel
+  get "/(*path)", to: "application#website", as: :website
 end
